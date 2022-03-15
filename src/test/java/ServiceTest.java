b7804c534b9444ecee94e4438a109cb836ca22ae
Lucut Andreea <andreea.lucut@gmail.com>
2022-03-15 16:10:06 +0200

    initial

---
+++ src/test/java/ServiceTest.java
@@ -0,0 +1,44 @@
+import domain.Student;
+import org.junit.Before;
+import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import static org.junit.Assert.*;
+
+public class ServiceTest {
+    public Service service;
+
+    @Before
+    public void init() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    @Test
+    public void addStudent_valid() {
+        Student validStudent = new Student("2778", "Manuela", 934, "man@yahoo.com");
+        Student result = service.addStudent(validStudent);
+        assertEquals(result, validStudent);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudent_invalid() {
+        Student validStudent = new Student("", "", -1, "");
+        service.addStudent(validStudent);
+    }
+}
